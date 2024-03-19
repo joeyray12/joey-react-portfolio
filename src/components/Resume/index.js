9d6455c8986340d8ee5b3bf15d99f748995703b0
joeyray12 <joeyraymond12497@gmail.com>
2024-03-19 10:55:03 -0400

    removing unused variables for deployment

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import myResume from "../../assets/Servicetitan photo.jpg";
-import { BsCloudDownload } from "react-icons/bs";
 import { MdFoundation } from "react-icons/md";
 import { GrTechnology } from "react-icons/gr";
 import { GrDocumentPerformance } from "react-icons/gr";
